7006195af93c6e5e0a1ef7d4cf9ffc9bc81cecf2
Daweihao84 <dwsamurai84@gmail.com>
2023-11-09 00:44:58 +0800

    2023/11/9: 1. HomePageVC UI set up-> Ok (Just missing Auto-Layout.)

--- IMS-Inventory/ViewController/HomePageViewController.swift
+++ IMS-Inventory/ViewController/HomePageViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class HomePageViewController: UIViewController {
 
+    let appTitleLabel: UILabel = UILabel()
     let userTitleLabel: UILabel = UILabel()
 
+    let goButton: UIButton = UIButton(type: .system)
 
-    let pickTextField: UITextField = UITextField()
+    let pickerViewTextField: UITextField = UITextField()
     let nameTextField: UITextField = UITextField()
 
     let pickerView: UIPickerView = UIPickerView()
@@ -27,52 +29,123 @@ class HomePageViewController: UIViewController {
         updateUI()
 
         print("Into HomePageVC")
+
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
 
     func updateUI () {
 
+        appTitleLabel.frame = CGRect(x: 108, y: 233, width: 215, height: 40)
+        appTitleLabel.text = "IMS Inventory"
+        appTitleLabel.font = UIFont.boldSystemFont(ofSize: 33)
+        appTitleLabel.textColor = .darkGray
+        appTitleLabel.numberOfLines = 0
+        appTitleLabel.adjustsFontSizeToFitWidth = true
+        appTitleLabel.textAlignment = .center
+        view.addSubview(appTitleLabel)
+
         self.view.backgroundColor = .white
 
-        userTitleLabel.frame = CGRect(x: 80, y: 303, width: 86, height: 26)
+        userTitleLabel.frame = CGRect(x: 65, y: 395, width: 90, height: 30)
         userTitleLabel.text = "填入姓名"
-        userTitleLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        userTitleLabel.font = UIFont.boldSystemFont(ofSize: 21)
         userTitleLabel.textColor = .darkGray
         userTitleLabel.numberOfLines = 0
         userTitleLabel.adjustsFontSizeToFitWidth = true
         view.addSubview(userTitleLabel)
 
-        // pickTextField
-        pickTextField.frame = CGRect(x: 80, y: 350, width: 270, height: 30)
-        pickTextField.placeholder = "選填你的名字"
-        pickTextField.inputView = pickerView
-        pickTextField.backgroundColor = UIColor.systemGray6
-        pickTextField.borderStyle = .roundedRect
-        pickTextField.keyboardType = .default
-        pickTextField.delegate = self
-        view.addSubview(pickTextField)
+        // pickerViewTextField
+        pickerViewTextField.frame = CGRect(x: 64, y: 446, width: 300, height: 50)
+        pickerViewTextField.placeholder = "選填你的名字"
+        pickerViewTextField.inputView = pickerView
+        pickerViewTextField.backgroundColor = UIColor.systemGray6
+        pickerViewTextField.borderStyle = .roundedRect
+        pickerViewTextField.keyboardType = .default
+        pickerViewTextField.delegate = self
+        view.addSubview(pickerViewTextField)
 
-//        nameTextField.placeholder = ""
+        pickerView(pickerViewTextField)
 
-        AddDelegate()
+        // goButton
+        let width: Int = 70
+        let height: Int = width
+        var config = UIButton.Configuration.plain()
+        config.background.image = UIImage(systemName: "arrowtriangle.right.fill")
+        config.background.imageContentMode = .center
+
+        goButton.tintColor = .white
+        goButton.configuration = config
+        goButton.frame = CGRect(x: 300, y: 800, width: width, height: height)
+        goButton.backgroundColor = .systemBlue
+        goButton.isUserInteractionEnabled = true
+        goButton.layer.cornerRadius = CGFloat(width / 2)
+        goButton.clipsToBounds = true
+        goButton.addTarget(self, action: #selector(goButtonTapped), for: .touchUpInside)
+        view.addSubview(goButton)
     }
 
+    func pickerView(_ : UITextField) {
 
-    // MARK: - Add delegate
-    func AddDelegate () {
-        pickTextField.delegate = self
-
-        // pickView
+        // UIPickerView
         pickerView.delegate = self
         pickerView.dataSource = self
+
+        // ToolBar
+        let toolBar = UIToolbar()
+        toolBar.barStyle = .default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.blue
+        toolBar.sizeToFit()
+
+        // Adding Button ToolBar
+        let doneButton = UIBarButtonItem(
+            title: "Done",
+            style: .plain,
+            target: self,
+            action: #selector(doneClick))
+
+        let spaceButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil)
+
+        let cancelButton = UIBarButtonItem(
+            title: "Cancel",
+            style: .plain,
+            target: self,
+            action: #selector(cancelClick))
+
+        toolBar.setItems([cancelButton, spaceButton, doneButton], animated: true)
+        toolBar.isUserInteractionEnabled = true
+        pickerViewTextField.inputAccessoryView = toolBar
+
     }
 
+    @objc func doneClick () {
+        pickerViewTextField.resignFirstResponder()
+        print("doneClick")
+    }
+
+    @objc func cancelClick () {
+        pickerViewTextField.resignFirstResponder()
+        print("cancelClick")
+    }
+
+    @objc func goButtonTapped () {
+        print("goButtonTapped")
+    }
 
 
 }
 
 
 // MARK: - Add Protocol
-extension HomePageViewController: UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+extension HomePageViewController: UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource, UIToolbarDelegate {
 
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -88,13 +161,18 @@ extension HomePageViewController: UITextFieldDelegate, UIPickerViewDelegate, UIP
     }
 
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        pickTextField.text = nameList[row]
-        pickerView.isHidden = true
+        pickerViewTextField.text      = nameList[row]
+        pickerViewTextField.textColor = .darkGray
+        pickerView.isHidden = false
     }
 
     private func textFieldDidBeginEditing(_ textField: UITextField) -> Bool {
         pickerView.isHidden = false
         print("textFieldDidBeginEditing")
         return false
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.pickerView(pickerViewTextField)
     }
 }
