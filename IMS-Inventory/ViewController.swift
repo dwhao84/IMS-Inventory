e544c404ad46c889d07d51d885dcc8ae0a26077d
Daweihao84 <dwsamurai84@gmail.com>
2023-11-08 16:30:25 +0800

    Initial Commit

---
+++ IMS-Inventory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMS-Inventory
+//
+//  Created by Dawei Hao on 2023/11/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
