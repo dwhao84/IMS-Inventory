80a49ce2767d5785ec16b90b69482c6c3501233b
Daweihao84 <dwsamurai84@gmail.com>
2024-05-18 23:58:23 +0800

    Add my Color library inside.

--- IMS-Inventory/ColorConstants.swift
+++ IMS-Inventory/ColorConstants.swift
@@ -5,4 +5,16 @@
 //  Created by Dawei Hao on 2024/5/18.
 //
 
-import Foundation
+import UIKit
+
+struct Colors {
+    static let darkGray: UIColor              = UIColor.darkGray
+    static let lightGray: UIColor             = UIColor.lightGray
+    static let black: UIColor                 = UIColor.black
+    static let white: UIColor                 = UIColor.white
+    static let blue: UIColor                  = UIColor.systemBlue
+    static let clear: UIColor                 = UIColor.clear
+    static let pink: UIColor                  = UIColor.systemPink
+    static let CustomTitleColor: UIColor      = UIColor(named: "CustomColor") ?? UIColor.white
+    static let CustomBackgroundColor: UIColor = UIColor(named: "CustomBackgroundColor") ?? Colors.black
+}
