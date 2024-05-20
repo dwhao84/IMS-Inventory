cf85e3051a347fd93df1de79f48688876fec4e95
Daweihao84 <dwsamurai84@gmail.com>
2024-05-20 21:48:15 +0800

    Add Custom font for Fonts.

--- IMS-Inventory/Instants/Fonts.swift
+++ IMS-Inventory/Instants/Fonts.swift
@@ -11,3 +11,5 @@ struct Fonts {
     static let NotoIKEA_TC_Bold: String    = "NotoIKEATraditionalChinese_Latin-Bold.ttf"
     static let NotoIKEA_TC_Regular: String = "NotoIKEATraditionalChinese_Latin-Regular.ttf"
 }
+
+
