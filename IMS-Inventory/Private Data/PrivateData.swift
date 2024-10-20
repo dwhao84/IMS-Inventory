60ec12ba534cdf957bc6dcc33144c0858c9ef1b8
Dawei Hao <dwsamurai84@gmail.com>
2024-10-21 00:42:23 +0800

    Create the private data for store private data.

---
+++ IMS-Inventory/Private Data/PrivateData.swift
@@ -0,0 +1,14 @@
+//
+//  PrivateData.swift
+//  IMS-Inventory
+//
+//  Created by Dawei Hao on 2024/10/21.
+//
+
+import UIKit
+
+class API {
+    private init() {}
+    static let baseUrl: String = "https://api.airtable.com/v0/app7877pVxbaMubQP/Table%201"
+    static let apiKey: String = "pat1LqVaDimhjw3Zf.b43913585307f91de54e59d08120eb228c2d09c777a0c10250ba3a5aab67bcf4"
+}
