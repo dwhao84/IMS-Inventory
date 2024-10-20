9765f5c05b102fcd722047c0bd9e71889d99861d
Dawei Hao <dwsamurai84@gmail.com>
2024-10-21 00:42:56 +0800

    Adjust UI for product collection view cell.

--- IMS-Inventory/ProductCollectionViewCell.swift
+++ IMS-Inventory/ProductCollectionViewCell.swift
@@ -18,11 +18,9 @@ class ProductCollectionViewCell: UICollectionViewCell {
       return customFont
     }
     
-    var productImageView: UIImageView = {
+    let productImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "photo.fill")
-        imageView.layer.borderWidth = 0.2
-        imageView.layer.borderColor = Colors.lightGray.cgColor
         imageView.tintColor = Colors.IKEA_Blue
         imageView.layer.cornerRadius = 10
         imageView.clipsToBounds = true
@@ -31,71 +29,62 @@ class ProductCollectionViewCell: UICollectionViewCell {
         return imageView
     }()
     
-    var articleNumberLabel: UILabel = {
-        let label = UILabel()
-        label.text = " No.10150 "
-        label.textColor = Colors.white // 使用系統顏色代替自定義顏色
-        label.font = scriptFont(size: 15)
-        label.backgroundColor = Colors.black
-        label.layer.cornerRadius = 2
-        label.clipsToBounds = true
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    let articleNumberTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "No.10150"
+        textView.textColor = Colors.white // 使用系統顏色代替自定義顏色
+        textView.font = scriptFont(size: 15)
+        textView.backgroundColor = Colors.black
+        textView.layer.cornerRadius = 2
+        textView.textContainerInset = UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2)
+        textView.clipsToBounds = true
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
     }()
     
-    var productENNameLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Wooden Shelf"
-        label.textColor = Colors.darkGray // 使用系統顏色代替自定義顏色
-        label.font = scriptFont(size: 13)
-        label.textAlignment = .left
-        label.numberOfLines = 2
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    var productTCNameLabel: UILabel = {
+    let productTCNameLabel: UILabel = {
         let label = UILabel()
         label.text = "木層板"
-        label.textColor = Colors.lightGray
-        label.font = scriptFont(size: 10)
+        label.textColor = Colors.darkGray
+        label.font = scriptFont(size: 15)
         label.textAlignment = .left
         label.numberOfLines = 2
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    var qtyLabel: UILabel = {
+    let productENNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Wooden Shelf"
+        label.textColor = Colors.lightGray // 使用系統顏色代替自定義顏色
+        label.font = scriptFont(size: 13)
+        label.textAlignment = .left
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let qtyLabel: UILabel = {
         let label = UILabel()
         label.text = "庫存數"
-        label.textColor = Colors.black // 使用系統顏色代替自定義顏色
+        label.textColor = Colors.lightGray // 使用系統顏色代替自定義顏色
         label.textAlignment = .left
         label.font = scriptFont(size: 13)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    var labelStackView: UIStackView = {
+    let labelStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.alignment = .leading
-        stackView.spacing = 5
+        stackView.spacing = 2
         stackView.distribution = .fill
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
     
-    var mainStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .vertical
-        stackView.alignment = .trailing
-        stackView.spacing = 5
-        stackView.distribution = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
+    // MARK: - Override init
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
@@ -106,57 +95,49 @@ class ProductCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    // MARK: - prepareForReuse
     override func prepareForReuse() {
         super.prepareForReuse()
         print("prepareForReuse")
     }
     
+    // MARK: - required init
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Set up UI:
     func setupUI() {
-        self.addSubview(productImageView)
-        self.addSubview(labelStackView)
-        self.addSubview(qtyLabel)
-        
-        configStackView()
+        contentView.addSubview(productImageView)
+        contentView.addSubview(labelStackView)
+        configLabelsStackView()
         addConstraints()
     }
     
-    func configStackView() {
-        articleNumberLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
-        qtyLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
-//        qtyLabel.backgroundColor = Colors.IKEA_Blue
-        
-        labelStackView.addArrangedSubview(productImageView)
-        labelStackView.addArrangedSubview(articleNumberLabel)
-        labelStackView.addArrangedSubview(productENNameLabel)
+    func configLabelsStackView() {
         labelStackView.addArrangedSubview(productTCNameLabel)
-        
-        mainStackView.addArrangedSubview(labelStackView)
-        mainStackView.addArrangedSubview(qtyLabel)
-        self.addSubview(mainStackView)
+        labelStackView.addArrangedSubview(productENNameLabel)
+        labelStackView.addArrangedSubview(articleNumberTextView)
+        labelStackView.addArrangedSubview(qtyLabel)
     }
     
     func addConstraints() {
         NSLayoutConstraint.activate([
+            productImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            productImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            productImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
             productImageView.widthAnchor.constraint(equalToConstant: 150),
-            productImageView.heightAnchor.constraint(equalTo: productImageView.widthAnchor, multiplier: 1),
-            labelStackView.widthAnchor.constraint(equalToConstant: 150),
-            mainStackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
-            mainStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
-            mainStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor)
+            productImageView.heightAnchor.constraint(equalToConstant: 150),
             
+            labelStackView.leadingAnchor.constraint(equalTo: productImageView.trailingAnchor, constant: 20),
+            labelStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            labelStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            labelStackView.bottomAnchor.constraint(lessThanOrEqualTo: contentView.bottomAnchor, constant: -30)
         ])
-    }
-    
-    private func configureCell () {
-        
     }
 }
 
-#Preview(traits: .fixedLayout(width: 200, height: 300), body: {
+#Preview(traits: .fixedLayout(width: 420, height: 170), body: {
     let prodictCollectionViewCell: UICollectionViewCell = ProductCollectionViewCell()
     return prodictCollectionViewCell
 })
