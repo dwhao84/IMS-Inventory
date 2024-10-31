2af8704e9523214823e5c1b823da4fd832dffee0
Dawei Hao <dwsamurai84@gmail.com>
2024-10-31 20:13:08 +0800

    Add Image source.

--- IMS-Inventory/Instants/Images.swift
+++ IMS-Inventory/Instants/Images.swift
@@ -8,8 +8,73 @@
 import UIKit
 
 struct Images {
+    // MARK: - Media & Photos
     static let image = UIImage(systemName: "photo.fill")!
+    static let camera = UIImage(systemName: "camera.fill")!
+    static let video = UIImage(systemName: "video.fill")!
+    static let photoLibrary = UIImage(systemName: "photo.on.rectangle.fill")!
+    static let favorite = UIImage(systemName: "heart.fill")!
+    
+    // MARK: - Organization & Lists
     static let list_bullet = UIImage(systemName: "list.bullet")!
+    static let folder = UIImage(systemName: "folder.fill")!
+    static let tag = UIImage(systemName: "tag.fill")!
+    static let bookmark = UIImage(systemName: "bookmark.fill")!
+    static let archive = UIImage(systemName: "archivebox.fill")!
+    
+    // MARK: - Settings & Tools
     static let gear = UIImage(systemName: "gear")!
     static let hammer = UIImage(systemName: "hammer")!
+    static let wrench = UIImage(systemName: "wrench.fill")!
+    static let slider = UIImage(systemName: "slider.horizontal.3")!
+    static let toggle = UIImage(systemName: "toggle.power")!
+    
+    // MARK: - Documents & Text
+    static let bookPages = UIImage(systemName: "book.pages")!
+    static let document = UIImage(systemName: "doc.fill")!
+    static let textDocument = UIImage(systemName: "doc.text.fill")!
+    static let clipboard = UIImage(systemName: "clipboard.fill")!
+    static let note = UIImage(systemName: "note.text")!
+    
+    // MARK: - Communication
+    static let message = UIImage(systemName: "message.fill")!
+    static let mail = UIImage(systemName: "envelope.fill")!
+    static let phone = UIImage(systemName: "phone.fill")!
+    static let bell = UIImage(systemName: "bell.fill")!
+    static let share = UIImage(systemName: "square.and.arrow.up")!
+    
+    // MARK: - User & Profile
+    static let person = UIImage(systemName: "person.fill")!
+    static let personCircle = UIImage(systemName: "person.circle.fill")!
+    static let group = UIImage(systemName: "person.3.fill")!
+    static let profile = UIImage(systemName: "person.crop.circle.fill")!
+    static let edit = UIImage(systemName: "pencil")!
+    
+    // MARK: - Security & Privacy
+    static let lock = UIImage(systemName: "lock.fill")!
+    static let unlock = UIImage(systemName: "lock.open.fill")!
+    static let shield = UIImage(systemName: "shield.fill")!
+    static let key = UIImage(systemName: "key.fill")!
+    static let fingerprint = UIImage(systemName: "touchid")!
+    
+    // MARK: - Navigation & Location
+    static let location = UIImage(systemName: "location.fill")!
+    static let map = UIImage(systemName: "map.fill")!
+    static let compass = UIImage(systemName: "location.north.circle.fill")!
+    static let home = UIImage(systemName: "house.fill")!
+    static let arrow = UIImage(systemName: "arrow.right.circle.fill")!
+    
+    // MARK: - Data & Cloud
+    static let cloud = UIImage(systemName: "cloud.fill")!
+    static let cloudUpload = UIImage(systemName: "icloud.and.arrow.up.fill")!
+    static let cloudDownload = UIImage(systemName: "icloud.and.arrow.down.fill")!
+    static let wifi = UIImage(systemName: "wifi")!
+    static let database = UIImage(systemName: "externaldrive.fill")!
+    
+    // MARK: - Time & Calendar
+    static let clock = UIImage(systemName: "clock.fill")!
+    static let calendar = UIImage(systemName: "calendar")!
+    static let timer = UIImage(systemName: "timer")!
+    static let alarm = UIImage(systemName: "alarm.fill")!
+    static let hourglass = UIImage(systemName: "hourglass")!
 }
