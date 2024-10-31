cd6b8bb8eaba7a4b46e24e978fcf94e250f8529e
Dawei Hao <dwsamurai84@gmail.com>
2024-10-31 20:14:24 +0800

    新增Padding Label的class，製作黑底白字。

--- IMS-Inventory/Components/PaddingLabel.swift
+++ IMS-Inventory/Components/PaddingLabel.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class PaddingLabel: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var padding = UIEdgeInsets(top: 3, left: 5, bottom: 3, right: 5)
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: padding))
     }
-    */
-
+    
+    override var intrinsicContentSize: CGSize {
+        let size = super.intrinsicContentSize
+        return CGSize(width: size.width + padding.left + padding.right,
+                     height: size.height + padding.top + padding.bottom)
+    }
 }
