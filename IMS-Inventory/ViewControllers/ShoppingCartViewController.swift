a962db073149b6f2af95a34158fc95f622acb6aa
Dawei Hao <dwsamurai84@gmail.com>
2024-11-03 00:59:49 +0800

    Create shopping cart vc.

--- IMS-Inventory/ViewControllers/ShoppingCartViewController.swift
+++ IMS-Inventory/ViewControllers/ShoppingCartViewController.swift
@@ -11,19 +11,10 @@ class ShoppingCartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = Colors.IKEA_Yellow
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+#Preview {
+    UINavigationController(rootViewController: ShoppingCartViewController())
 }
