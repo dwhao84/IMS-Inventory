896cfead785448c3f74df3d78914a9f15216e4d9
Dawei Hao <dwsamurai84@gmail.com>
2024-11-03 00:59:16 +0800

    Add constraints, and set up stackViews.

--- IMS-Inventory/ViewControllers/ProductDetailViewController.swift
+++ IMS-Inventory/ViewControllers/ProductDetailViewController.swift
@@ -22,17 +22,6 @@ class ProductDetailViewController: UIViewController {
         return imageView
     } ()
     
-    let productDetailInfoLabel: UILabel = {
-        let label: UILabel = UILabel()
-        label.text = ""
-        label.textColor = Colors.darkGray
-        label.font = UIFont.systemFont(ofSize: 15)
-        label.textAlignment = .left
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    } ()
-        
     let articleNumberLabel: PaddingLabel = {
         let label = PaddingLabel()
         label.text = "No.10150"
@@ -46,22 +35,22 @@ class ProductDetailViewController: UIViewController {
         return label
     } ()
     
-    let supplierLabel: UILabel = {
+    let productDetailInfoLabel: UILabel = {
         let label: UILabel = UILabel()
-        label.text = "Which Supplier"
+        label.text = "貨架重量: kg"
         label.textColor = Colors.darkGray
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.font = UIFont.systemFont(ofSize: 20)
         label.textAlignment = .left
-        label.numberOfLines = 1
+        label.numberOfLines = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     } ()
     
-    let technicalSpecsLabel: UILabel = {
+    let stockValueLabel: UILabel = {
         let label: UILabel = UILabel()
-        label.text = ""
+        label.text = "庫存數:"
         label.textColor = Colors.darkGray
-        label.font = UIFont.systemFont(ofSize: 15)
+        label.font = UIFont.systemFont(ofSize: 20)
         label.textAlignment = .left
         label.numberOfLines = 3
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -83,11 +72,97 @@ class ProductDetailViewController: UIViewController {
         return stepper
     } ()
     
-    let labelsStackView: UIStackView = {
+    let qtyLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "1"
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
+    
+    let stepperStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    } ()
+    
+    let statusLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "狀態選擇"
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
+    
+    let statusTextField: UITextField = {
+        let tf: UITextField = UITextField()
+        tf.text = "請選擇狀態"
+        tf.borderStyle = .roundedRect
+        tf.textColor = Colors.darkGray
+        tf.font = UIFont.systemFont(ofSize: 20)
+        tf.clearButtonMode = .whileEditing
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    } ()
+    
+    let statusStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    } ()
+    
+    let dateLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.text = "使用日期"
+        label.textColor = Colors.darkGray
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    } ()
+    
+    let dateSelectTextField: UITextField = {
+        let tf: UITextField = UITextField()
+        tf.text = "請選擇日期"
+        tf.textColor = Colors.darkGray
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 20)
+        tf.clearButtonMode = .whileEditing
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    } ()
+    
+    let dateStackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.alignment = .center
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    } ()
+    
+    let mainStackView: UIStackView = {
         let stackView: UIStackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .fill
-        stackView.alignment = .center
+        stackView.alignment = .leading
+        stackView.spacing = 30
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     } ()
@@ -99,24 +174,27 @@ class ProductDetailViewController: UIViewController {
         return scrollView
     } ()
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setupUI()
+        addTargets()
     }
     
+    // MARK: - setupUI
     func setupUI() {
-        configBackgroundColor()
+        setBackgroundColor()
         setNavigationView()
+        configStackViews()
         addConstraints()
     }
     
-    func configBackgroundColor () {
+    // MARK: - set BackgroundColor
+    func setBackgroundColor () {
         self.view.backgroundColor = Colors.white
     }
     
+    // MARK: - set NavigationView
     func setNavigationView () {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.largeTitleDisplayMode = .always
@@ -135,25 +213,105 @@ class ProductDetailViewController: UIViewController {
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
     
+    // MARK: - add Constraints
     func addConstraints() {
-        self.view.addSubview(productImageView)
-        self.view.addSubview(articleNumberLabel)
-        NSLayoutConstraint.activate([
-            productImageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            productImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20),
-            productImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
-            productImageView.heightAnchor.constraint(equalTo: productImageView.widthAnchor, multiplier: 0.7),
-            articleNumberLabel.topAnchor.constraint(equalTo: productImageView.bottomAnchor, constant: 20),
-            articleNumberLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20)
-        ])
+        // 1. Add subviews
+        view.addSubview(scrollView)
+        scrollView.addSubview(mainStackView)
         
-        self.view.addSubview(confirmBtn)
+        [dateSelectTextField, statusTextField].forEach {
+            $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
+            $0.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        }
+        
         NSLayoutConstraint.activate([
-            confirmBtn.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
-            confirmBtn.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 30),
-            confirmBtn.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -30),
-            confirmBtn.heightAnchor.constraint(equalToConstant: 60)
+            // ScrollView constraints
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            // MainStackView constraints
+            mainStackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: 30),
+            mainStackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor, constant: 20),
+            mainStackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor, constant: -20),
+            mainStackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor, constant: -30),
+            // Make stack view width equal to scroll view width minus padding
+            mainStackView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor, constant: -40),
+            
+            // ProductImageView constraints
+            productImageView.heightAnchor.constraint(equalTo: productImageView.widthAnchor, multiplier: 0.7),
+            productImageView.widthAnchor.constraint(equalToConstant: 300),
+            productImageView.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor),
+            productImageView.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor),
+            
+            // ConfirmButton constraints
+            confirmBtn.leadingAnchor.constraint(equalTo: mainStackView.leadingAnchor, constant: 10),
+            confirmBtn.trailingAnchor.constraint(equalTo: mainStackView.trailingAnchor, constant: -10),
+            confirmBtn.heightAnchor.constraint(equalToConstant: 60),
+            confirmBtn.bottomAnchor.constraint(equalTo: mainStackView.bottomAnchor, constant: -20)
         ])
+    }
+    
+    func configStackViews () {
+        // 使用時間
+        dateStackView.addArrangedSubview(dateLabel)
+        dateStackView.addArrangedSubview(dateSelectTextField)
+        
+        // 選擇狀態
+        statusStackView.addArrangedSubview(statusLabel)
+        statusStackView.addArrangedSubview(statusTextField)
+        
+        stepperStackView.addArrangedSubview(stepper)
+        stepperStackView.addArrangedSubview(qtyLabel)
+        
+        mainStackView.addArrangedSubview(productImageView)
+        mainStackView.addArrangedSubview(articleNumberLabel)
+        mainStackView.addArrangedSubview(dateStackView)
+        mainStackView.addArrangedSubview(statusStackView)
+        mainStackView.addArrangedSubview(productDetailInfoLabel)
+        mainStackView.addArrangedSubview(stockValueLabel)
+        mainStackView.addArrangedSubview(stepperStackView)
+        mainStackView.addArrangedSubview(confirmBtn)
+    }
+    
+    // MARK: - add Targets
+    func addTargets () {
+        confirmBtn.addTarget(self, action: #selector(confirmBtnTapped), for: .touchUpInside)
+        stepper.addTarget(self, action: #selector(stepperTapped), for: .touchUpInside)
+    }
+    
+    // MARK: - Actions
+    @objc func confirmBtnTapped (_ sender: UIButton) {
+        print("confirmBtnTapped")
+        let shoppingCartVC = ShoppingCartViewController()
+        shoppingCartVC.modalPresentationStyle = .overFullScreen
+        self.navigationController?.pushViewController(shoppingCartVC, animated: true)
+    }
+    
+    @objc func stepperTapped(_ sender: UIStepper) {
+        print("Stepper Tapped")
+        qtyLabel.text = "\(Int(stepper.value))"
+        print(qtyLabel.text!)
+    }
+    
+}
+
+extension ProductDetailViewController: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textField Did Begin Editing")
+        textField.becomeFirstResponder()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textField Did End Editing")
+        textField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("textField Should Return")
+        return true
     }
 }
 
