ffc4fa02545559b67f809fe4a8650515c7d6d0d5
Dawei Hao <dwsamurai84@gmail.com>
2024-11-04 22:54:39 +0800

    Create Product Image Cell.

---
+++ IMS-Inventory/Components/ProductImageCell.swift
@@ -0,0 +1,42 @@
+//
+//  ProductImageCell.swift
+//  IMS-Inventory
+//
+//  Created by Dawei Hao on 2024/11/3.
+//
+
+import UIKit
+
+class ProductImageCell: UITableViewCell {
+    private let productImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.tintColor = Colors.black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(productImageView)
+        NSLayoutConstraint.activate([
+            productImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            productImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            productImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            productImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+    
+    func configure(with image: UIImage?) {
+        productImageView.image = image
+    }
+}
---
+++ IMS-Inventory/Components/ProductQuantityCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductQuantityCell.swift
+//  IMS-Inventory
+//
+//  Created by Dawei Hao on 2024/11/4.
+//
+
+import UIKit
+
+class ProductQuantityCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
