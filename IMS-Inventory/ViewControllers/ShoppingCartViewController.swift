754189b75060837ec57bfbc4a17a53e01ca602ca
Dawei Hao <dwsamurai84@gmail.com>
2024-11-21 23:02:40 +0800

    Add tableview in shopping cart view controller.

--- IMS-Inventory/ViewControllers/ShoppingCartViewController.swift
+++ IMS-Inventory/ViewControllers/ShoppingCartViewController.swift
@@ -8,10 +8,42 @@
 import UIKit
 
 class ShoppingCartViewController: UIViewController {
-
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = Colors.IKEA_Yellow
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension ShoppingCartViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1 // Replace with your data count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        // Configure your cell here
+        return cell
     }
 }
 
