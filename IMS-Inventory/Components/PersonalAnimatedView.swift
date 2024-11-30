d517b2dfda23b3d967280d6ea830a02a974568f2
Dawei Hao <dwsamurai84@gmail.com>
2024-11-30 16:02:44 +0800

    Adjust Peronal Animated View.

--- IMS-Inventory/Components/PersonalAnimatedView.swift
+++ IMS-Inventory/Components/PersonalAnimatedView.swift
@@ -2,26 +2,24 @@ import SwiftUI
 import UIKit
 
 struct PersonalAnimatedView: View {
-    static let person_badge = UIImage(systemName: "person.badge.plus")!
-    @State private var isAnimating = false
-    @State private var showBadge = false
+    @State private var scale: CGFloat = 1.0
     
     var body: some View {
-        SwiftUI.Image(systemName: showBadge ? "person.badge.plus" : "person")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 100, height: 100)
-            .scaleEffect(isAnimating ? 1.2 : 1.0)
-            .animation(.bouncy(duration: 1), value: isAnimating)
-            .onAppear {
-                // 設定定時器來自動切換圖示
-                Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
-                    withAnimation(.spring()) {
-                        showBadge.toggle()
-                        isAnimating.toggle()
+        VStack {
+            SwiftUI.Image(systemName: "person")
+                .resizable().scaledToFit()
+                .imageScale(.large)
+                .foregroundStyle(Color.black)
+                .frame(width: 150, height: 130)
+                .scaleEffect(scale)
+                .onAppear {
+                    withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: true)) {
+                        scale = 0.9
                     }
                 }
-            }
+            Text("Create your account")
+                .bold()
+        }
     }
 }
 
