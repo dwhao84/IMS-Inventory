418c8f5dff82c8d73ee990bc4bd9f609f2759a2f
Dawei Hao <dwsamurai84@gmail.com>
2024-11-30 14:52:36 +0800

    Add Personal Animated View.

--- IMS-Inventory/Components/PersonalAnimatedView.swift
+++ IMS-Inventory/Components/PersonalAnimatedView.swift
@@ -1,15 +1,27 @@
-//
-//  PersonalAnimatedView.swift
-//  IMS-Inventory
-//
-//  Created by Dawei Hao on 2024/11/30.
-//
-
 import SwiftUI
+import UIKit
 
 struct PersonalAnimatedView: View {
+    static let person_badge = UIImage(systemName: "person.badge.plus")!
+    @State private var isAnimating = false
+    @State private var showBadge = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        SwiftUI.Image(systemName: showBadge ? "person.badge.plus" : "person")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 100, height: 100)
+            .scaleEffect(isAnimating ? 1.2 : 1.0)
+            .animation(.bouncy(duration: 1), value: isAnimating)
+            .onAppear {
+                // 設定定時器來自動切換圖示
+                Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { _ in
+                    withAnimation(.spring()) {
+                        showBadge.toggle()
+                        isAnimating.toggle()
+                    }
+                }
+            }
     }
 }
 
