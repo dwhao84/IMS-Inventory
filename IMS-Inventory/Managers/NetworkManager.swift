0da889d10bf17c437e68ba44bfa16fe8553c9837
Dawei Hao <dwsamurai84@gmail.com>
2024-12-01 00:48:28 +0800

    Create NetworkMgr for get

--- IMS-Inventory/Managers/NetworkManager.swift
+++ IMS-Inventory/Managers/NetworkManager.swift
@@ -5,13 +5,56 @@
 //  Created by Dawei Hao on 2024/11/28.
 //
 
-import UIKit
+import Foundation
 
 class NetworkManager {
-    
-    private init () {}
+    private init() {}
     static let shared = NetworkManager()
     
+    func getProductData(completion: @escaping(Result<[Record], NetworkError>) -> Void) {
+        let url = API.baseUrl
+        var request = URLRequest(url: url)
+        request.setValue("Bearer \(API.apiKey)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.networkError(error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Response: \(String(describing: response))")
+                completion(.failure(.invalidResponse))
+                return
+            }
+
+            print("Status code: \(httpResponse.statusCode)")
+            print("Headers: \(httpResponse.allHeaderFields)")
+            
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                if let jsonString = String(data: data, encoding: .utf8) {
+                    print("JSON Response:", jsonString)
+                }
+                let rackingData = try decoder.decode(RackingData.self, from: data)
+                completion(.success(rackingData.records))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }
+        
+        task.resume()
+    }
     
-    
+    enum NetworkError: Error {
+        case networkError(Error)
+        case invalidResponse
+        case noData
+        case decodingError(Error)
+    }
 }
