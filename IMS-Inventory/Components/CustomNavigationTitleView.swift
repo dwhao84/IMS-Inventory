1e3cac07c8da58e24f8ff821294f17e6e8723031
Dawei Hao <dwsamurai84@gmail.com>
2024-12-02 01:29:27 +0800

    Make a class for Custom Nav title view.

--- IMS-Inventory/Components/CustomNavigationTitleView.swift
+++ IMS-Inventory/Components/CustomNavigationTitleView.swift
@@ -5,4 +5,46 @@
 //  Created by Dawei Hao on 2024/12/2.
 //
 
-import Foundation
+// CustomNavigationTitleView.swift
+import UIKit
+
+class CustomNavigationTitleView: UIView {
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = Colors.black
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // 初始化方法
+    init(title: String) {
+        super.init(frame: .zero)
+        titleLabel.text = title
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            // 設置標題標籤的約束
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor), // 靠左對齊
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor), // 垂直置中
+            
+            // 設置視圖本身的寬度
+            self.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width - 50)
+        ])
+    }
+    
+    // 提供一個方法來更新標題
+    func updateTitle(_ title: String) {
+        titleLabel.text = title
+    }
+}
