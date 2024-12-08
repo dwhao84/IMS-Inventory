4fecbfac03fa88118c45dc58e07f3d51b340526f
Dawei Hao <dwsamurai84@gmail.com>
2024-12-08 21:21:46 +0800

    Add api key and api url.

--- IMS-Inventory/Private Data/PrivateData.swift
+++ IMS-Inventory/Private Data/PrivateData.swift
@@ -10,12 +10,13 @@ import UIKit
 struct API {
     private init() {}
     static let baseUrl               = URL(string: "https://api.airtable.com/v0/app7877pVxbaMubQP/Racking_Info")!
-    static let apiKey: String        = "patXaY8Vhmqi3dtB6.93b9b04767d52acf0c66e9df2ec67db7901bd05f0485cf77cc19f2ca2fc17d65"
+    static let apiKey: String        = "patuJC6rtLI3iGdS1.87ac91b245f91e71624b0f7e001cc0d1a9e30bcfa3846f94a4d0e2d1e4241494"
+    static let apiKeyBearerToken: String = "Bearer patuJC6rtLI3iGdS1.79bc27090cef6919d6b1adc031ca33afc7ff0f5da0bad7550a71ec870ab56c68"
     static let authorization: String = "Authorization"
-    
-    static let imsDatabaseUrl        = URL(string: "https://api.airtable.com/v0/app7877pVxbaMubQP/Racking_Info")!
+
     static let contentType: String   = "Content-Type"
     static let application: String   = "application/json"
     
-    static let get_IMSDatabase_url = URL(string: "https://api.airtable.com/v0/app7877pVxbaMubQP/Racking_Info/recz4rejYdGOdjQeL")!
+    static let get_IMSDatabase_url = URL(string: "https://api.airtable.com/v0/app7877pVxbaMubQP/Racking_Database/")!
+    static let delete_IMSDatabase_url = URL(string: "https://api.airtable.com/v0/app7877pVxbaMubQP/Racking_Database/recIV4mmRSgPkgODk")!
 }
