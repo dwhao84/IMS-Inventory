6971c2afc3b6244163378cfd38578a07a32fe655
Dawei Hao <dwsamurai84@gmail.com>
2024-12-08 21:24:19 +0800

    Build Cart structure.

--- IMS-Inventory/Model/Cart.swift
+++ IMS-Inventory/Model/Cart.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 
-struct RackingDatabase {
-    
+enum BorrowReturn {
     struct Response: Codable {
         let records: [Record]
     }
-    
+
     struct Record: Codable {
         let id: String
         let createdTime: String
@@ -20,41 +19,20 @@ struct RackingDatabase {
     }
 
     struct Fields: Codable {
-        let qty: Int?
-        let articleName: String?
-        let category: String?
         let articleNumber: String?
-        let image: [ImageInfo]?
+        let rackingDescription: String?
+        let orderNumber: String?
+        let createdDate: String?
+        let status: String?
+        let rackingQty: Int?
         
         enum CodingKeys: String, CodingKey {
-            case qty = "Qty"
-            case articleName
-            case category = "Category"
-            case articleNumber
-            case image
+            case articleNumber = "article_number"
+            case rackingDescription = "racking_description"
+            case orderNumber = "order number"
+            case createdDate = "created date"
+            case status
+            case rackingQty = "racking qty"
         }
-    }
-
-    struct ImageInfo: Codable {
-        let id: String
-        let width: Int
-        let height: Int
-        let url: String
-        let filename: String
-        let size: Int
-        let type: String
-        let thumbnails: Thumbnails
-    }
-
-    struct Thumbnails: Codable {
-        let small: ThumbnailInfo
-        let large: ThumbnailInfo
-        let full: ThumbnailInfo
-    }
-
-    struct ThumbnailInfo: Codable {
-        let url: String
-        let width: Int
-        let height: Int
     }
 }
