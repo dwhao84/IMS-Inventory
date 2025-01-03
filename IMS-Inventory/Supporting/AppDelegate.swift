a9a96c7cf3a4aa0d640844049b4b5e157f7d5b90
Dawei Hao <dwsamurai84@gmail.com>
2025-01-03 23:43:50 +0800

    Add keep presist log into the iOS app.

--- IMS-Inventory/Supporting/AppDelegate.swift
+++ IMS-Inventory/Supporting/AppDelegate.swift
@@ -13,19 +13,25 @@ import IQKeyboardManagerSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var authStateHandle: AuthStateDidChangeListenerHandle?
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        // FirebaseApp
         FirebaseApp.configure()
         FirebaseConfiguration.shared.setLoggerLevel(.error)
-        
         IQKeyboardManager.shared.isEnabled = true
         
+        authStateHandle = Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user {
+                print("=== User is signIn ===")
+            } else {
+                print("=== User is signOut ===")
+            }
+        }
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
