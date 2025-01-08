1ec479165183440f29325750970c0eeabb42cb50
Dawei Hao <dwsamurai84@gmail.com>
2025-01-08 23:52:55 +0800

    Add User noticifation center.

--- IMS-Inventory/Supporting/AppDelegate.swift
+++ IMS-Inventory/Supporting/AppDelegate.swift
@@ -5,45 +5,138 @@
 //  Created by Dawei Hao on 2023/11/8.
 //
 
+//
+//  AppDelegate.swift
+//  IMS-Inventory
+//
+//  Created by Dawei Hao on 2023/11/8.
+//
+
 import UIKit
 import Firebase
 import FirebaseAuth
+import UserNotifications
 import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    // MARK: - Properties
+    
     var authStateHandle: AuthStateDidChangeListenerHandle?
-
+    
+    // MARK: - Application Lifecycle
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        FirebaseApp.configure()
-        FirebaseConfiguration.shared.setLoggerLevel(.error)
-        IQKeyboardManager.shared.isEnabled = true
-        
-        authStateHandle = Auth.auth().addStateDidChangeListener { auth, user in
-            if user != nil {
-                print("=== User is signIn ===")
-            } else {
-                print("=== User is signOut ===")
-            }
-        }
+        setupFirebase()
+        setupKeyboardManager()
+        setupAuthStateListener()
+        setupPushNotifications(application)
         return true
     }
     
-    // MARK: UISceneSession Lifecycle
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    // MARK: - Setup Methods
+    
+    private func setupFirebase() {
+        FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.error)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    
+    private func setupKeyboardManager() {
+        IQKeyboardManager.shared.isEnabled = true
+        IQKeyboardManager.shared.resignOnTouchOutside = true
     }
-
-
+    
+    private func setupAuthStateListener() {
+        authStateHandle = Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user {
+                print("=== User is signIn ===")
+                print("User ID: \(user.uid)")
+                // 可以在這裡處理用戶登入後的其他邏輯
+            } else {
+                print("=== User is signOut ===")
+                // 可以在這裡處理用戶登出後的其他邏輯
+            }
+        }
+    }
+    
+    private func setupPushNotifications(_ application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions) { granted, error in
+                if let error = error {
+                    print("推播通知授權失敗: \(error.localizedDescription)")
+                    return
+                }
+                
+                if granted {
+                    print("使用者已允許推播通知")
+                    DispatchQueue.main.async {
+                        application.registerForRemoteNotifications()
+                    }
+                } else {
+                    print("使用者拒絕推播通知")
+                }
+            }
+    }
+    
+    // MARK: - UISceneSession Lifecycle
+    
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
+    }
+    
+    func application(_ application: UIApplication,
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // 當場景被丟棄時釋放相關資源
+    }
+    
+    // MARK: - Push Notification Registration
+    
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("註冊推播失敗: \(error.localizedDescription)")
+    }
+    
+    // MARK: - Memory Management
+    
+    deinit {
+        if let handle = authStateHandle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
 }
 
+// MARK: - UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // 當應用程式在前景時收到通知
+        completionHandler([.banner, .badge, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        // 處理使用者點擊通知的事件
+        let userInfo = response.notification.request.content.userInfo
+        print("使用者點擊通知: \(userInfo)")
+        completionHandler()
+    }
+}
